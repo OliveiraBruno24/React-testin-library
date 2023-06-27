ae7dc3aadc546dddd00d3b4266a9fd460d1f5607
Bruno de Oliveira <brunodeoliveira2000@hotmail.com>
2023-06-27 06:25:33 -0300

    requisito 4

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,14 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+
+import App from '../App';
+
+test('Verifica a renderização daa página NotFound', () => {
+  renderWithRouter(<App />, { route: '/trem' });
+  const notFound = screen.getByRole('heading', { name: /page requested not found/i });
+  expect(notFound).toBeInTheDocument();
+
+  const img = screen.getByRole('img', { name: /Pikachu crying because the page requested was not found/i });
+  expect(img).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  expect(img).toBeInTheDocument();
+});
