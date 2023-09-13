faa6830279db395bdac1f112fb2923294bb65c37
Bruno de Oliveira <brunodeoliveira2000@hotmail.com>
2023-09-12 22:00:35 -0300

    publicando projeto finalizado

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, screen, waitFor } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
